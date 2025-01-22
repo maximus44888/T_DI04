cc7e8519fc4641aa01af5972cf9d98b7500214d6
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-01-22 20:00:37 +0100

    renamed files

--- src/main/java/dam/alumno/filmoteca/MainApp.java
+++ src/main/java/dam/alumno/filmoteca/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
