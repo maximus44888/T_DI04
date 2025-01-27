c57a6175c941fd6d1ae82b5fb2c08e93235d087e
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-01-28 00:42:19 +0100

    changed main window title

--- src/main/java/dam/alumno/filmoteca/MainApp.java
+++ src/main/java/dam/alumno/filmoteca/MainApp.java
@@ -19,7 +19,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Filmoteca");
         stage.setScene(scene);
         stage.show();
     }
