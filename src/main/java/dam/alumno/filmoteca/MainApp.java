a498434e8c28db63f6d126d5d5ba84c50fa963c3
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-01-28 01:36:53 +0100

    variables for json src/dest files

--- src/main/java/dam/alumno/filmoteca/MainApp.java
+++ src/main/java/dam/alumno/filmoteca/MainApp.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class MainApp extends Application {
+    private final File src = new File("datos/peliculas.json");
+    private final File dest = new File("datos/peliculas2.json");
 
     public static void main(final String[] args) {
         launch();
@@ -24,10 +26,10 @@ public class MainApp extends Application {
     }
 
     public void init() {
-        System.out.println("Cargando datos desde fichero datos/peliculas.json");
+        System.out.println("Cargando datos desde " + src);
 
         try {
-            System.out.println(FilmArchive.getInstance().loadFrom(new File("datos/peliculas.json")));
+            System.out.println(FilmArchive.getInstance().loadFrom(src));
         } catch (final IOException e) {
             System.out.println("ERROR al cargar los datos. La aplicación no puede iniciarse");
             e.printStackTrace();
@@ -36,11 +38,13 @@ public class MainApp extends Application {
     }
 
     public void stop() {
+        System.out.println("Guardando datos en " + dest);
+
         final var filmArchive = FilmArchive.getInstance();
         System.out.println(filmArchive.films);
 
         try {
-            filmArchive.saveTo(new File("datos/peliculas2.json"));
+            filmArchive.saveTo(dest);
         } catch (final IOException e) {
             System.out.println("ERROR no se ha podido guardar los datos de la aplicación");
             e.printStackTrace();
